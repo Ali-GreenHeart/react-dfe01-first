24f4f34bef30686d56e3f06c105fd534879f4a7c
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-14 18:20:17 +0400

    sum with useeffect localstorage

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,23 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 
 export default function Input() {
-    const [n1, setN1] = useState('')
-    const [n2, setN2] = useState('')
+    const [n1, setN1] = useState()
+    const [n2, setN2] = useState()
+
+    // life-cycle 
+    useEffect(() => {
+        setN1(+localStorage.getItem('n1Value'))
+        setN2(+localStorage.getItem('n2Value'))
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('n1Value', n1)
+    }, [n1])
+    useEffect(() => {
+        localStorage.setItem('n2Value', n2)
+    }, [n2])
 
     const inputRef = useRef()
+
     return (
         <>
             <input
