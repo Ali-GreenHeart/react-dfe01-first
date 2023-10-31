b995897a7ff7f0efc32a4894d1e87372271f4fd9
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-31 17:59:45 +0400

    seperate actions

--- src/pages/UsersWR/index.jsx
+++ src/pages/UsersWR/index.jsx
@@ -1,39 +1,13 @@
 import axios from "axios"
-import { useEffect, useReducer, useState } from "react"
+import { useEffect, useReducer } from "react"
 import PageContainer from "../../components/PageContainer"
-import { v4 } from 'uuid'
+import { USERS_ACTIONS, reducer } from "./reducer"
 const _url = "https://jsonplaceholder.typicode.com/users"
 
-const USERS_ACTIONS = {
-    get_users: 0,
-    add_user: 1,
-    rm_last_user: 2,
-    rm_user: 3
-}
-
-const reducer = (store, action) => {
-    switch (action.type) {
-        case USERS_ACTIONS.get_users:
-            return action.payload;
-        case USERS_ACTIONS.add_user:
-            const username = prompt('enter new username')
-            const name = prompt('enter new name')
-            const id = v4()
-            return [...store, { username, name, id }]
-        case USERS_ACTIONS.rm_last_user:
-            let a = [...store]
-            a.pop()
-            return a;
-        case USERS_ACTIONS.rm_user:
-            const newUsers = store.filter((us) => us.id !== action.payload)
-            return newUsers;
-        default:
-            return store;
-    }
-}
 
 const UserWR = () => {
     const [users, dispatch] = useReducer(reducer, [])
+
     useEffect(() => {
         axios.get(_url).then(({ data }) => {
             dispatch({ payload: data, type: USERS_ACTIONS.get_users })
@@ -47,7 +21,7 @@ const UserWR = () => {
                 users.map(({ id, username, name }) => {
                     return (
                         <div key={id}>
-                            <h1>{username} -  {name} -  <button onClick={() => dispatch({ type: USERS_ACTIONS.rm_user, payload: id })}>❌</button></h1>
+                            <h1>{username} -  {name} - <button onClick={() => dispatch({ type: USERS_ACTIONS.edit_user, payload: id })}>edit the user</button>  -<button onClick={() => dispatch({ type: USERS_ACTIONS.rm_user, payload: id })}>❌</button></h1>
                         </div>
                     )
                 })
---
+++ src/pages/UsersWR/reducer.jsx
@@ -0,0 +1,38 @@
+import { v4 } from "uuid";
+
+export const USERS_ACTIONS = {
+    get_users: 0,
+    add_user: 1,
+    rm_last_user: 2,
+    rm_user: 3,
+    edit_user: 4
+}
+
+export const reducer = (store, action) => {
+    switch (action.type) {
+        case USERS_ACTIONS.get_users:
+            return action.payload;
+        case USERS_ACTIONS.add_user:
+            const username = prompt('enter new username')
+            const name = prompt('enter new name')
+            const id = v4()
+            return [...store, { username, name, id }]
+        case USERS_ACTIONS.rm_last_user:
+            let a = [...store]
+            a.pop()
+            return a;
+        case USERS_ACTIONS.rm_user:
+            const newUsers = store.filter((us) => us.id !== action.payload)
+            return newUsers;
+        case USERS_ACTIONS.edit_user:
+            const _newUsers = store.map((user) => {
+                if (user.id === action.payload) {
+                    return { ...user, username: prompt('enter new username', user.username) }
+                }
+                return user;
+            })
+            return _newUsers
+        default:
+            return store;
+    }
+}
