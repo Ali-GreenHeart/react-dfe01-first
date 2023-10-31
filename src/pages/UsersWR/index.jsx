9aee8307ea26c7327cb6c64505289c957d13f2ef
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-31 17:21:31 +0400

    add actions object

--- src/pages/UsersWR/index.jsx
+++ src/pages/UsersWR/index.jsx
@@ -4,22 +4,31 @@ import PageContainer from "../../components/PageContainer"
 import { v4 } from 'uuid'
 const _url = "https://jsonplaceholder.typicode.com/users"
 
-const reducer = (users, action) => {
+const USERS_ACTIONS = {
+    get_users: 0,
+    add_user: 1,
+    rm_last_user: 2,
+    rm_user: 3
+}
+
+const reducer = (store, action) => {
     switch (action.type) {
-        case 'get_users':
+        case USERS_ACTIONS.get_users:
             return action.payload;
-        case 'add_user':
+        case USERS_ACTIONS.add_user:
             const username = prompt('enter new username')
             const name = prompt('enter new name')
             const id = v4()
-            return [...users, { username, name, id }]
-        case "rm_last_user":
-            let a = [...users]
+            return [...store, { username, name, id }]
+        case USERS_ACTIONS.rm_last_user:
+            let a = [...store]
             a.pop()
             return a;
-        case 'rm_user':
-            const newUsers = users.filter((us) => us.id !== action.payload)
+        case USERS_ACTIONS.rm_user:
+            const newUsers = store.filter((us) => us.id !== action.payload)
             return newUsers;
+        default:
+            return store;
     }
 }
 
@@ -27,18 +36,18 @@ const UserWR = () => {
     const [users, dispatch] = useReducer(reducer, [])
     useEffect(() => {
         axios.get(_url).then(({ data }) => {
-            dispatch({ payload: data, type: 'get_users' })
+            dispatch({ payload: data, type: USERS_ACTIONS.get_users })
         })
     }, [])
     return (
         <PageContainer>
-            <button onClick={() => dispatch({ type: 'add_user' })}>add a user</button>
-            <button onClick={() => dispatch({ type: 'rm_last_user' })}>remove last user</button>
+            <button onClick={() => dispatch({ type: USERS_ACTIONS.add_user })}>add a user</button>
+            <button onClick={() => dispatch({ type: USERS_ACTIONS.rm_last_user })}>remove last user</button>
             {
                 users.map(({ id, username, name }) => {
                     return (
                         <div key={id}>
-                            <h1>{username} -  {name} -  <button onClick={() => dispatch({ type: 'rm_user', payload: id })}>❌</button></h1>
+                            <h1>{username} -  {name} -  <button onClick={() => dispatch({ type: USERS_ACTIONS.rm_user, payload: id })}>❌</button></h1>
                         </div>
                     )
                 })
