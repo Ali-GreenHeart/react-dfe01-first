96c7bb666e002b73f6b90e44f54ae15dc4e0765c
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-21 18:16:30 +0400

    add pagination

--- src/pages/NetPro/components/SelectedCategoryInfo/index.jsx
+++ src/pages/NetPro/components/SelectedCategoryInfo/index.jsx
@@ -1,12 +1,14 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Pagination, Typography } from "@mui/material"
+import { useState } from "react"
 
 const SelectedCategoryInfo = ({ title, body, categoryItems = [] }) => {
+    const [page, setPage] = useState(0)
     return (
         <Box>
             <Typography variant="h6" fontWeight={600} color="#435072">{title}</Typography>
             <Typography variant="body">{body}</Typography>
             {
-                categoryItems.map(({ id, img, title, body }) => {
+                categoryItems.slice(page * 3, page * 3 + 3).map(({ id, img, title, body }) => {
                     return (
                         <Box key={id}>
                             <img style={{ width: '100px', height: '100px' }} src={img} alt="" />
@@ -16,6 +18,7 @@ const SelectedCategoryInfo = ({ title, body, categoryItems = [] }) => {
                     )
                 })
             }
+            <Pagination onChange={(event, value) => setPage(value - 1)} count={categoryItems.length / 3} variant="outlined" shape="rounded" />
         </Box>
     )
 }
--- src/pages/NetPro/db.js
+++ src/pages/NetPro/db.js
@@ -6,15 +6,75 @@ const items = [
         body: "0Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac, massa nec pulvinar nunc, nulla a sed. Sapien massa lacus, mi, egestas metus, vitae non metus. Cras lorem quisque in iaculis tortor pharetra est dolor. Enim ipsum hac pulvinar dolor. Congue ut viverra id sagittis vulputate sed posuere.",
         categoryItems: [
             {
-                id: 0,
+                id: 12134,
                 img: "eziz-xan.jpg",
-                title: 'Liebert EXM2 Uninterruptible Power SupplyD',
+                title: '12134 -> 0Liebert EXM2 Uninterruptible Power SupplyD',
                 body: 'Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
             },
             {
-                id: 1,
+                id: 12133,
                 img: "eziz-xan.jpg",
-                title: 'Soyutma sistemleri',
+                title: '12133 -> 1Soyutma sistemleri',
+                body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 2,
+                img: "eziz-xan.jpg",
+                title: '2 -> 10Liebert EXM2 Uninterruptible Power SupplyD',
+                body: 'Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 3,
+                img: "eziz-xan.jpg",
+                title: '3 -> 11Soyutma sistemleri',
+                body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 4,
+                img: "eziz-xan.jpg",
+                title: '4 -> 20Liebert EXM2 Uninterruptible Power SupplyD',
+                body: 'Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 5,
+                img: "eziz-xan.jpg",
+                title: '5 -> 21Soyutma sistemleri',
+                body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 6,
+                img: "eziz-xan.jpg",
+                title: '6 -> 30Liebert EXM2 Uninterruptible Power SupplyD',
+                body: 'Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 7,
+                img: "eziz-xan.jpg",
+                title: '7 -> 31Soyutma sistemleri',
+                body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 8,
+                img: "eziz-xan.jpg",
+                title: '8 -> 40Liebert EXM2 Uninterruptible Power SupplyD',
+                body: 'Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 9,
+                img: "eziz-xan.jpg",
+                title: '9Soyutma s10 -> istemleri',
+                body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 10,
+                img: "eziz-xan.jpg",
+                title: '10Liebert E11 -> XM2 Uninterruptible Power SupplyD',
+                body: 'Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
+            },
+            {
+                id: 11,
+                img: "eziz-xan.jpg",
+                title: '11Soyutma s12 -> istemleri',
                 body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
             },
         ]
@@ -33,7 +93,7 @@ const items = [
             {
                 id: 1,
                 img: "eziz-xan.jpg",
-                title: 'Soyutma sistemleri',
+                title: '1 -> Soyutma sistemleri',
                 body: '12Vertiv™ Liebert® EXM2 is a monolithic uninterruptible power supply(UPS), designed to address the rising need of highly reliable and efficient UPS for next - gen mid- size critical applications.D'
             },
         ]
